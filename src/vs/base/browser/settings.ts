374b60930d3ba200eaf5b0e4032e8b839b31876d
ricbet <qingyi.xjh@antgroup.com>
2023-12-26 13:57:13 +0800

    chore: cherry-pick 0.47.0 patch

---
+++ src/vs/base/browser/settings.ts
@@ -0,0 +1,26 @@
+/* eslint-disable header/header */
+
+class OverflowContentWidgetsSettings {
+	private _TOP_PADDING = 22;
+	private _BOTTOM_PADDING = 22;
+
+	set topPadding(num: number) {
+		if (Number.isInteger(num)) {
+			this._TOP_PADDING = num;
+		}
+	}
+	get topPadding() {
+		return this._TOP_PADDING;
+	}
+
+	set bottomPadding(num: number) {
+		if (Number.isInteger(num)) {
+			this._BOTTOM_PADDING = num;
+		}
+	}
+	get bottomPadding() {
+		return this._BOTTOM_PADDING;
+	}
+}
+
+export const overflowWidgetsSettings = new OverflowContentWidgetsSettings();
