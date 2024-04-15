e01993c3d90563e1fff18b68ed90f69e0cfbe7e2
野声 <lijiacheng.ljc@antgroup.com>
2024-04-15 15:41:57 +0800

    Remove patches

--- apply-patches.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_PATH=$(realpath "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-
-wget https://github.com/opensumi/monaco-editor-core/pull/14.patch
-git am --keep-cr --signoff <  14.patch
-rm 14.patch
